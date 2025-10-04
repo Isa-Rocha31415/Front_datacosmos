540d2b5030bc8093bc7ccbc314e54784146d9dec
cali4nication <gallegosmetzli@gmail.com>
2025-10-04 13:58:35 -0600

    carpetas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href={favicon} />
 </svelte:head>
 
 {@render children?.()}
---
+++ src/routes/main.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+    import Main from "../components/Main.svelte";
+</script>
+<Main></Main>
