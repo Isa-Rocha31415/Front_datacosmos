f507ec59004a03128856e4322f7679bd44327842
Isabel <isabelrocha31415@gmail.com>
2025-10-04 13:53:18 -0600

    creando el header y rutas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
+import header from './components/header.svelte'
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
