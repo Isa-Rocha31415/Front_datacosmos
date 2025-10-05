4644889826daf4f82d89b7e959199f7e7eb6dfc0
cali4nication <gallegosmetzli@gmail.com>
2025-10-04 19:21:59 -0600

    try again

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,7 @@
 </script>
 
 <svelte:head>
-<<<<<<< HEAD
 	<link rel="icon" type="image/png" href={favicon} />
-=======
-  <link rel="icon" href={favicon} />
->>>>>>> 778d8e401b9dcc6c0a09a0f96c0537b593676b5e
 </svelte:head>
 
 <Header />
